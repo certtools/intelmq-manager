c74ea1b4102485758ae1e2ed7c4590f027c3521d
Mauro Silva <mauro.a.silva@gmail.com>
2014-08-12 10:17:37 +0100

    Removed unnecessary PHP files (remainder of old version)

--- php/removeedge.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-
-    unset($_SESSION["data"]["edges"][$_GET["id"]]);
-
-    $encoded_data = json_encode($_SESSION["data"]);
-    file_put_contents('/tmp/botnet_data', $encoded_data);
-?>
