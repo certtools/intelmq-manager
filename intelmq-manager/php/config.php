59a072c0520812dcdd5aeb1eea8b926d0f1a441f
Mauro Silva <mauro.a.silva@gmail.com>
2015-07-09 14:41:01 +0100

    Fixed whitespace issues

--- intelmq-manager/php/config.php
+++ intelmq-manager/php/config.php
@@ -2,8 +2,8 @@
 
     $FILES = array(
         'bots' 		=> '/opt/intelmq/etc/BOTS',
-	'defaults' 	=> '/opt/intelmq/etc/defaults.conf',
-	'harmonization' => '/opt/intelmq/etc/harmonization.conf',
+        'defaults' 	=> '/opt/intelmq/etc/defaults.conf',
+        'harmonization' => '/opt/intelmq/etc/harmonization.conf',
         'pipeline' 	=> '/opt/intelmq/etc/pipeline.conf',
         'runtime' 	=> '/opt/intelmq/etc/runtime.conf',
         'startup' 	=> '/opt/intelmq/etc/startup.conf',
