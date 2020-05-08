412f1ed438e212a290e3ee2dbb4dae03c776a282
Bernhard Herzog <bernhard.herzog@intevation.de>
2020-05-08 22:12:14 +0200

    Add stand-alone server interface.
    The new module lets us run hug's builtin HTTP-server to serve all of
    IntelMQ-Manager, including the static files.

---
+++ intelmq_manager/serve.py
@@ -0,0 +1,44 @@
+"""Stand-alone server for IntelMQ-Manager
+
+SPDX-FileCopyrightText: 2020 Intevation GmbH <https://intevation.de>
+SPDX-License-Identifier: AGPL-3.0-or-later
+
+Funding: of initial version by SUNET
+Author(s):
+  * Bernhard Herzog <bernhard.herzog@intevation.de>
+
+This is the main entry point when serving all of the IntelMQ-Manager via
+hug. In addition to the backend implemented in intelmq_manager.api, this
+module also serves the static files (JS, CSS, etc.).
+
+Run with hug from the shell as:
+   hug -f intelmq_manager/serve.py
+"""
+
+import os
+
+import hug # type: ignore
+
+import intelmq_manager.api
+
+
+@hug.extend_api()
+def add_api():
+    return [intelmq_manager.api]
+
+
+@hug.static("/")
+def static_dirs():
+    return [os.path.join(os.path.dirname(__file__),
+                         os.pardir, "intelmq-manager")]
+
+
+@hug.startup()
+def setup(api):
+    """Initialize the API when hug starts.
+
+    This function calls initialize_api passing the value of the
+    environment variable INTELMQ_MANAGER_CONFIG as the name of the
+    configuration file.
+    """
+    intelmq_manager.api.initialize_api(os.environ.get("INTELMQ_MANAGER_CONFIG"))
