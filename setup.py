b550a39552ff2e4f09c9b80a261ec89a09e90d2d
Bernhard Herzog <bernhard.herzog@intevation.de>
2020-05-08 22:37:32 +0200

    Add rudimentary seup.py file
    The setup.py file is incomplete and cannot really be used to install the
    software yet. The main reason this is committed is to start documenting
    the minimum requirements.

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+from intelmq_manager.version import __version__
+
+
+setup(
+    name="intelmq-manager",
+    version=__version__,
+    python_requires='>=3.5',
+    install_requires=[
+        "hug>=2.3.0",
+        ],
+    packages=find_packages(),
+    url='https://github.com/certtools/intelmq-manager/',
+    description=("IntelMQ Manager is a graphical interface to manage"
+                 " configurations for the IntelMQ framework."),
+)
