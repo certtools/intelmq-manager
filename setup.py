133142647b0bd7ea9148a2d11a7cb6f205f38a70
Birger Schacht <schacht@cert.at>
2020-11-20 09:55:52 +0100

    Build the html files when creating the source distribution
    This is implemented using a custom `sdist` class in setup.py
    that generates the files using the mako template engine.
    This also updates the MANIFEST.in file to include the html files
    in the source distribution (and also cleans it up a bit).

--- setup.py
+++ setup.py
@@ -1,17 +1,59 @@
 #!/usr/bin/python3
 from setuptools import find_packages, setup
+from setuptools.command.sdist import sdist as _sdist
+
+import pathlib
+import shutil
+from mako.lookup import TemplateLookup
 
 from intelmq_manager.version import __version__
 
+class CustomSdist(_sdist):
+    """ Custom source dist command to build html files."""
+
+    template_dir = pathlib.Path("intelmq_manager/templates")
+    template_lookup = TemplateLookup(directories=[template_dir], default_filters=["h"])
+
+
+    def render_page(self, pagename: str) -> str:
+        template = self.template_lookup.get_template(pagename + ".mako")
+        controller_cmd = "intelmq"
+        allowed_path = "/opt/intelmq/var/lib/bots/"
+        return template.render(pagename=pagename,
+                controller_cmd=controller_cmd,
+                allowed_path=allowed_path)
+
+    def build(self):
+        outputdir = pathlib.Path('intelmq_manager/html')
+        outputdir.mkdir(parents=True, exist_ok=True)
+
+        htmlfiles = ["configs", "management", "monitor", "check", "about", "index"]
+        for filename in htmlfiles:
+            print("Rendering {}.html".format(filename))
+            html = self.render_page(filename)
+            with outputdir.joinpath("{}.html".format(filename)) as p:
+                p.write_text(html)
+
+        staticfiles = ["css", "images", "js", "plugins", "less"]
+        for filename in staticfiles:
+            print("Copying {} recursively".format(filename))
+            src = pathlib.Path('intelmq_manager/static/{}'.format(filename))
+            dst = outputdir.joinpath(filename)
+            if dst.exists():
+                shutil.rmtree(dst)
+            shutil.copytree(src, dst)
+
+    def run(self):
+        self.build()
+        return _sdist.run(self)
 
 setup(
+    cmdclass = {
+        'sdist': CustomSdist,
+        },
     name="intelmq-manager",
     version=__version__,
     python_requires='>=3.5',
-    install_requires=[
-        "hug>=2.3.0",
-        "mako>=1.0.7",
-        ],
     packages=find_packages(),
     include_package_data=True,
     url='https://github.com/certtools/intelmq-manager/',
