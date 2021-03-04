80d3ed6e08f41ec2b104d8432686b82fce9cc9dd
Sebastian Wagner <wagner@cert.at>
2021-03-04 12:50:50 +0100

    REL: 2.3.0 Feature release

--- intelmq_manager/version.py
+++ intelmq_manager/version.py
@@ -3,5 +3,5 @@
 SPDX-FileCopyrightText: 2020 Intelmq Team <intelmq-team@cert.at>
 SPDX-License-Identifier: AGPL-3.0-or-later
 """
-__version_info__ = (2, 3, 0, 'dev1')
+__version_info__ = (2, 3, 0)
 __version__ = '.'.join(map(str, __version_info__))
